da20f747dc4d3e798523fce46ff2f2701abdeb0d
enfrte <enfrte@gmail.com>
2021-08-24 22:12:09 +0300

    Removed some obsolete code

--- background.js
+++ background.js
@@ -1,15 +1,3 @@
-// Listen for messages
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting === "hello")
-      sendResponse({farewell: "goodbye"});
-  }
-);
-
-
 let active = true;
 
 // Called when the extension UI button is clicked
@@ -21,7 +9,7 @@ chrome.action.onClicked.addListener((tab) => {
 		})
 		.then(() => {
 			chrome.action.setIcon({path: 'images/icon48_on.png', tabId: tab.id}); // Change icon for current tab only
-			console.log("background.js executeScript injected the foreground script");
+			//console.log("background.js executeScript injected the foreground script");
 		})
 		.catch(err => console.log('executeScript error:', err));
 	} 
