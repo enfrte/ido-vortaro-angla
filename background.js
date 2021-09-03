777400765bc19107c91c0a4ac53f9f9e5c0689b1
enfrte <enfrte@gmail.com>
2021-09-04 00:05:10 +0300

    Refactor

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 // Called when the extension UI button is clicked
 chrome.action.onClicked.addListener((tab) => {
-		chrome.scripting.executeScript({
-			target: { tabId: tab.id, allFrames: true },
-			files: ['foreground.js']
-		})
-		.then(() => {
-			chrome.action.setIcon({path: 'images/icon48_on.png', tabId: tab.id}); // Change icon for current tab only
-		})
-		.catch(err => console.log('executeScript error:', err));
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id, allFrames: true },
+		files: ['foreground.js']
+	})
+	.then(() => {
+		chrome.action.setIcon({path: 'images/icon48_on.png', tabId: tab.id}); // Change icon for current tab only
+	})
+	.catch(err => console.log('executeScript error:', err));
 });
